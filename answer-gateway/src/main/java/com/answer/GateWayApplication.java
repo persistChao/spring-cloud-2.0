44c16f30689cd7e3e0835fdb43387fbd9bb60d53
persistChao <suchao1@kingsoft.com>
2021-09-29 10:39:26 +0800

    init project

---
+++ answer-gateway/src/main/java/com/answer/GateWayApplication.java
@@ -0,0 +1,16 @@
+package com.answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author answer
+ * @version 1.0.0
+ * @date 2021/9/28 8:47 下午
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
---
+++ answer-gateway/src/main/java/com/answer/config/GateWayConfig.java
@@ -0,0 +1,30 @@
+package com.answer.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author answer
+ * @version 1.0.0
+ * @date 2021/9/28 4:02 下午
+ */
+@Configuration
+public class GateWayConfig {
+    /**
+     * 手动配置路由规则
+     * 配置了一个id为baidu_service的路由规则，
+     * 当访问地址http://localhost:9527/guonei的时候会自动转发到 http://new.baidu.con/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
+
+    @Bean(name = "routeLocator")
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("baidu_service", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+}
